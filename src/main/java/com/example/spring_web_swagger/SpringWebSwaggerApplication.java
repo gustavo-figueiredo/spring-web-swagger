d908299682d6c33e1e540c47a2ef70cb8b0d0b75
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-21 10:09:18 -0300

    first commit

---
+++ src/main/java/com/example/spring_web_swagger/SpringWebSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_web_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSwaggerApplication.class, args);
+	}
+
+}
