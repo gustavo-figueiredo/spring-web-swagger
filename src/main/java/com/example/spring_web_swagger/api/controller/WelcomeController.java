faaaf645b7f6abc27943f174658d5dd1a9b25f59
Gustavo Figueiredo <gf1985@gmail.com>
2024-08-21 11:41:16 -0300

    move WelcomeController

--- src/main/java/com/example/spring_web_swagger/api/controller/WelcomeController.java
+++ src/main/java/com/example/spring_web_swagger/api/controller/WelcomeController.java
@@ -1,10 +1,11 @@
-package com.example.spring_web_swagger.api;
+package com.example.spring_web_swagger.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
     @GetMapping
     public String welcome() {
         return "Welcome to Spring Web Swagger API";
